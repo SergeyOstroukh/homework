5d2d7d1203cdb33c9cd67c8b961bdeb9efeed249
SergeyOstroukh <hardmass3ffect@gmail.com>
2023-01-15 00:40:47 +0300

    решил 4 задачу task08

---
+++ task08/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите целое число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int counter = 1;
+
+
+// while (counter < num)
+// {
+//     if (counter % 2 == 0)
+//     {
+//         counter++;
+//     }
+//     else
+//     {
+//         Console.Write(counter + " ");
+//         counter++;
+//     }
+
+// }
+
+
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+
+
+while (counter < num)
+{
+    if (counter % 2 == 0)
+    {
+       Console.Write(counter + " ");
+        counter++;
+    }
+    else
+    {
+        
+        counter++;
+    }
+
+}
